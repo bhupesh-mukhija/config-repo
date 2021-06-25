1e41d3970ce15713208cf5d36daddca28d16b2b0
Mukhija <bhupesh.mukhija@sage.com>
2021-06-25 14:32:04 +0100

    Commit all files

--- testfolder/entrypoint.sh
+++ testfolder/entrypoint.sh
@@ -4,12 +4,15 @@ echo "Hello $1"
 echo "Hello $2"
 time=$(date)
 echo "::set-output name=time::$time"
+# setting path, this is not working in actions runner
 PATH=~/sfdx/bin:$PATH
+# creating  alias in case path is not set in actions runner
+alias sfdx=/root/sfdx/bin/sfdx
 which jq
 jq --version
 #which sfdx
 /root/sfdx/bin/sfdx --version
-alias sfdx=/root/sfdx/bin/sfdx
+
 sfdx --version
 echo "Current"
 ls -l
@@ -19,7 +22,20 @@ echo "github/home"
 ls -l /github/home
 echo "github/workflow"
 ls -l /github/workflow
+echo "github/workspace"
+ls -l /github/workspace
 echo "../"
 ls -l ../
 echo "../../"
 ls -l ../../
+echo "/root"
+ls -l /root/
+echo "/root/root"
+ls -l /root/root
+
+TARGETDEVHUBUSERNAME="devhubuser"
+echo "Authorize Devhub..."
+echo $TARGETDEVHUBUSERNAME
+echo $1 > /root/secrets/devhub.txt
+sfdx auth:sfdxurl:store --sfdxurlfile=/root/secrets/devhub.txt --setalias=$TARGETDEVHUBUSERNAME
+sfdx force:org:list --all
