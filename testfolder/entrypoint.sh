49f03e543b21541d667b6c3c6ac90a28d6c45af4
Mukhija <bhupesh.mukhija@sage.com>
2021-06-29 13:19:46 +0100

    Update entrypoint json

--- testfolder/entrypoint.sh
+++ testfolder/entrypoint.sh
@@ -22,41 +22,39 @@ sfdx auth:sfdxurl:store --sfdxurlfile=/root/secrets/devhub.txt --setalias=$TARGE
 SFDX_JSON=$(</github/workspace/sfdx-project.json)
 # TODO: DISABLE FOR ACTION
 #SFDX_JSON=$(<../../sfdx-project.json)
-P_NAME=$(jq ".name" <<< $SFDX_JSON)
-# TODO: DISABLE FOR ACTION
-#P_NAME="salesforce-global-sales"
+P_NAME=$(echo $SFDX_JSON | jq -r ".packageDirectories | map(select(.default == true))  | .[0].package")
 PACKAGE_INFO=$(queryPackageByName $P_NAME)
 echo $PACKAGE_INFO
 echo "**************************************************************************************************"
-#QUERY_STATUS=$(jq ".status" <<< $PACKAGE_INFO)
-if [ "$(jq ".status" <<< $PACKAGE_INFO)" = "1" ]
+#QUERY_STATUS=$(echo $PACKAGE_INFO | jq -r ".status")
+if [ "$(echo $PACKAGE_INFO | jq -r ".status")" = "1" ]
 then
     echo "Query Error FAIL THE JOB"
-    echo $(jq ".name,.message,.stack" <<< $PACKAGE_INFO)
+    echo $PACKAGE_INFO | jq -r ".name,.message,.stack"
     exit 1
     # TODO: ERROR HANDLING - send emails or post to teams channel
 else
     echo "PACKAGE QUERY SUCCESSFUL"
-    #RETURN_SIZE=$(jq ".result.totalSize" <<< $PACKAGE_INFO)
-    if [ "$(jq ".result.totalSize" <<< $PACKAGE_INFO)" = "0" ]
+    #RETURN_SIZE=$(echo $PACKAGE_INFO | jq ".result.totalSize")
+    if [ "$(echo $PACKAGE_INFO | jq ".result.totalSize")" = "0" ]
     then
         echo "$P_NAME not found"
         # TODO: CREATE PACKAGE BEFORE CREATING VERSION
     else
         echo "$P_NAME found, continue to create package version"
-        P_VERSION_DEVHUB=$(jq -r '"\(.result.records[0].MajorVersion)"+"."+"\(.result.records[0].MinorVersion)"+"."+"\(.result.records[0].PatchVersion)"' <<< $PACKAGE_INFO)
+        P_VERSION_DEVHUB=$(echo $PACKAGE_INFO | jq -r '"\(.result.records[0].MajorVersion)"+"."+"\(.result.records[0].MinorVersion)"+"."+"\(.result.records[0].PatchVersion)"')
         echo $P_VERSION_DEVHUB
-        P_VERSION_SFDX_JSON=$(cut -d "." -f1,2,3 <<< $(jq -r ".packageDirectories | map(select(.package == \"$P_NAME\")) | .[0].versionNumber" <<< $SFDX_JSON))
+        P_VERSION_SFDX_JSON=$(echo $SFDX_JSON | jq -r ".packageDirectories | map(select(.package == \"$P_NAME\")) | .[0].versionNumber" | cut -d "." -f1,2,3)
         echo $P_VERSION_SFDX_JSON
-        echo $(jq -r ".result.records[0].IsReleased" <<< $PACKAGE_INFO)
         #if [ $P_VERSION_DEVHUB = $P_VERSION_SFDX_JSON ] && [ $(jq -r ".result.records[0].IsReleased" <<< $PACKAGE_INFO) = "true" ];
         if [ $P_VERSION_DEVHUB = $P_VERSION_SFDX_JSON ]
         then
             echo "Devhub version equals sfdx json version"
-            if [ $(jq -r ".result.records[0].IsReleased" <<< $PACKAGE_INFO) = "true" ]
+            echo $(echo $PACKAGE_INFO | jq -r ".result.records[0].IsReleased")
+            if [ $(echo $PACKAGE_INFO | jq -r ".result.records[0].IsReleased") = "true" ]
             then
                 # TODO: generate error: requested version already have a released major.minor.patch
-                echo "ERROR: Requested version is already released, please update and rerun the job"
+                echo "ERROR: Requested version is already released, please update sfdx project json and rerun the job"
             fi
         else
             echo "Devhub version not equals sfdx json version"
