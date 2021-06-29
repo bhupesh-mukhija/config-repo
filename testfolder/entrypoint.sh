f931159ef9888f88e7cc9197f8b16e945fcd958b
Mukhija <bhupesh.mukhija@sage.com>
2021-06-29 13:58:22 +0100

    Updated entrypoint

--- testfolder/entrypoint.sh
+++ testfolder/entrypoint.sh
@@ -8,17 +8,18 @@ queryPackageByName() {
 }
 
 # setting path, this is not working in actions runner
-PATH=~/sfdx/bin:$PATH
-# creating alias in case path is not set in actions runner
-alias sfdx=/root/sfdx/bin/sfdx
+PATH=/root/sfdx/bin:$PATH
 sfdx --version
+# creating alias in case path is not set in actions runner
+# does not work in
+alias sfdx=/root/sfdx/bin/sfdx
 /root/sfdx/bin/sfdx --version
 TARGETDEVHUBUSERNAME="devhubuser"
 # TODO: DISABLE FOR ACTION
 #TARGETDEVHUBUSERNAME="sagegroup"
 echo "Authorize Devhub..."
 echo $1 > /root/secrets/devhub.txt
-sfdx auth:sfdxurl:store --sfdxurlfile=/root/secrets/devhub.txt --setalias=$TARGETDEVHUBUSERNAME
+/root/sfdx/bin/sfdx auth:sfdxurl:store --sfdxurlfile=/root/secrets/devhub.txt --setalias=$TARGETDEVHUBUSERNAME
 SFDX_JSON=$(</github/workspace/sfdx-project.json)
 # TODO: DISABLE FOR ACTION
 #SFDX_JSON=$(<../../sfdx-project.json)
