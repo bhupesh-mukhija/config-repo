42a5b970b720acb75a0565345afc8c2838217bc5
Mukhija <bhupesh.mukhija@sage.com>
2021-06-30 12:54:29 +0100

    Update createpackage

--- scripts/ci/createPackage.sh
+++ scripts/ci/createPackage.sh
@@ -39,6 +39,7 @@ packageCreate() {
                 fi
             else
                 echo "Devhub version not equals sfdx json version"
+                ls -l
                 # TODO: only allow minor/major/patch versions upgrades
                 createVersion --sourcepath $(echo $SFDX_JSON | jq -r ".packageDirectories | map(select(.default == true))  | .[0].path") \
                     --package $P_NAME --tag $(git rev-parse --short "$GITHUB_SHA") --targetdevhubusername $TARGETDEVHUBUSERNAME --wait 30 --definitionfile $DEFINITIONFILE
