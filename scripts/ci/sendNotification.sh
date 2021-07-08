027f07fcbb675277ae78a4168399a42c73a23b64
Mukhija <bhupesh.mukhija@sage.com>
2021-07-08 22:44:14 +0100

    Test

--- scripts/ci/sendNotification.sh
+++ scripts/ci/sendNotification.sh
@@ -3,5 +3,5 @@
 source "$SCRIPTS_PATH/config/scripts/ci/notificationutil.sh"
 TITLE="DX Job Run Status"
 SUBTITLE="Job run failed"
-sendNotification --statuscode "1" --message "Unknown error occured" \
+sendNotificationWoStatus --statuscode "1" --message "Unknown error occured" \
                 --details "Error occured during package creation, please check dev channel or view logs by clicking View Log button"
