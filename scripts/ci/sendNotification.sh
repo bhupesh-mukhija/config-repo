6d030333f2aaac9f0edf0385a7c19a0f87bcea23
Mukhija <bhupesh.mukhija@sage.com>
2021-07-08 22:29:45 +0100

    Test

--- scripts/ci/sendNotification.sh
+++ scripts/ci/sendNotification.sh
@@ -2,5 +2,6 @@
 # send teams notification on job failure
 source "$SCRIPTS_PATH/config/scripts/ci/notificationutil.sh"
 ls -l
+pwd
 sendNotification --statuscode "1" --message "Unknown error occured" \
                 --details "Error occured during package creation, please check dev channel or view logs by clicking View Log button"
