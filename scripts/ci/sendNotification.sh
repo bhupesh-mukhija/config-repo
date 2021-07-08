e40ef2dedc0ab13dc98feb71823a3c2071ff47e1
Mukhija <bhupesh.mukhija@sage.com>
2021-07-08 22:41:28 +0100

    Test

--- scripts/ci/sendNotification.sh
+++ scripts/ci/sendNotification.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # send teams notification on job failure
 source "$SCRIPTS_PATH/config/scripts/ci/notificationutil.sh"
-ls -l
-pwd
+TITLE="DX Job Run Status"
+SUBTITLE="Job run failed"
 sendNotification --statuscode "1" --message "Unknown error occured" \
                 --details "Error occured during package creation, please check dev channel or view logs by clicking View Log button"
