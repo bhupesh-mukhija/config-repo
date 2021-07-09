8750a4df31a79cf64eaf80413e820f569d8e0ba4
Mukhija <bhupesh.mukhija@sage.com>
2021-07-09 14:28:06 +0100

    Update

--- scripts/ci/install.sh
+++ scripts/ci/install.sh
@@ -7,10 +7,12 @@ function installPackage() {
     local PACKAGE_NAME=$(cat $SCRIPTS_PATH/sfdx-project.json | jq -r ".packageDirectories | map(select(.default == true))  | .[0].package")
     # query package from devhub
     #QUERY_RESPONSE=$(queryPackageByName1 $PACKAGE_NAME)
-    local SUBSCRIBER_PACKAGE_VERSION=$(echo $(queryPackageByName1 $PACKAGE_NAME) | jq -r '.result.records[0].SubscriberPackageVersion.attributes.url' | sed 's/.*\///')
+    local SUBSCRIBER_PACKAGE_VERSION=$(echo $(queryPackageByName $PACKAGE_NAME) | jq -r '.result.records[0].SubscriberPackageVersion.attributes.url' | sed 's/.*\///')
     echo $SUBSCRIBER_PACKAGE_VERSION
     # get package report for details
     local PACKAGE_REPORT=$(sfdx force:package:version:report --targetdevhubusername=$TARGETDEVHUBUSERNAME -p $SUBSCRIBER_PACKAGE_VERSION --json)
+    handleSfdxResponse "$PACKAGE_REPORT"
+
     echo "Package to be installed"
     echo $PACKAGE_REPORT | jq # TODO: Parse json and show formatted
     # TODO: Make it async use loop to report on create
